819b36c7ba2c1b70a2bc2aefed3f05c0ec59fc8b
jaypatel7212 <jaypatel.lamprostech@gmail.com>
2023-05-31 16:43:23 +0530

    Added Weekly Task

---
+++ week2project/client/index.js
@@ -0,0 +1,30 @@
+const axios = require("axios");
+const niceList = require("../utils/niceList.json");
+const MerkleTree = require("../utils/MerkleTree");
+const prompt = require("prompt-sync")();
+
+const serverUrl = "http://localhost:1225";
+
+async function main() {
+  // TODO: how do we prove to the server we're on the nice list?
+  const merkleTree = new MerkleTree(niceList);
+  const root = merkleTree.getRoot();
+
+  const name = prompt("which name do you want to check?");
+  if (name == null) {
+    alert("You have to type a name");
+  }
+
+  const index = niceList.findIndex((n) => n === name);
+  const proof = merkleTree.getProof(index);
+
+  const { data: gift } = await axios.post(`${serverUrl}/gift`, {
+    // TODO: add request body parameters here!
+    name,
+    proof,
+  });
+
+  console.log({ gift });
+}
+
+main();
